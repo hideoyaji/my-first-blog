19c3b865fc2b25061aa3206bbaf5723ad979344c
hideo <koneaya@gmail.com>
2022-03-27 11:53:59 +0900

    settings.py, css

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9hg1r=!a!ic^yoi$ugvtviv7*_w-h06j=51a1$yd3q6e#s(zw2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
